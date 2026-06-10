7b359024b3a5257d78c4e95adaa76ef30c58fed0
Roel-Junior Alejo Viernes <rjviernes620@gmail.com>
2026-06-10 22:32:37 +0100

    end product goes to build

--- vite.config.ts
+++ vite.config.ts
@@ -16,8 +16,13 @@ function figmaAssetResolver() {
   }
 }
 
+
 export default defineConfig({
   base: '/juicegels_v2/',
+
+  build: {
+    outDir: 'docs',
+  },
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
