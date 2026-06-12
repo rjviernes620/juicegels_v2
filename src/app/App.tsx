6d779c10db12832ac33642530eead6126dd24b3a
Roel-Junior Alejo Viernes <rjviernes620@gmail.com>
2026-06-12 02:24:16 +0100

    proper pointer link to payment server

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -332,7 +332,7 @@ const updateQty = (idx: number, delta: number) => {
     setIsSubmitting(true);
 
     const endpoint = import.meta.env.DEV
-      ? "https://juicegels-backend.onrender.com/create-checkout-session"
+      ? "https://juicegels-v2.onrender.com/create-checkout-session"
       : "/create-checkout-session";
 
     try {
