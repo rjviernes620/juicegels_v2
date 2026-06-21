6e2d4341df1df5e7949cd5ae480fe5ab5e74cc6a
Roel-Junior Alejo Viernes <rjviernes620@gmail.com>
2026-06-21 12:30:38 +0100

    rebuilt access to sanity for external

--- studio-juicegels_back/sanity.cli.ts
+++ studio-juicegels_back/sanity.cli.ts
@@ -10,6 +10,7 @@ export default defineCliConfig({
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
+    appId: 'g3qxv59xckjk3dk6bjo71za3',
     autoUpdates: true,
   }
 })
